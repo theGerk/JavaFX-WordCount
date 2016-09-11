5ace8dd1b7817c59d0de7fd1ab238d7fe82786a3
Benji <benji@altmansoftwaredesign.com>
2016-09-10 22:22:57 -0500

    got layout working

---
+++ WordCount/src/wordcount/GUIController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wordcount;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
+
+/**
+ *
+ * @author Benji
+ */
+public class GUIController implements Initializable {
+
+	@FXML
+	private ListView myList;
+
+	@FXML
+	private void handleButtonAction(ActionEvent event) {
+		try {
+			List<String> text = Files.readAllLines(new FileChooser().showOpenDialog(myList.getScene().getWindow()).toPath());
+			for (String t : text) {
+				System.out.println(t);
+			}
+		} catch (IOException ex) {
+			//netbeans generated code...
+			//may want to change this line up later
+			Logger.getLogger(GUIController.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO
+	}
+
+}
